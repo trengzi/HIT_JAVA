a3aad51b588899ca5ad57d0645b1bee1aac2a617
trengzi <trangnguyen16022003@gmail.com>
2023-08-15 12:10:15 +0700

    change

--- btvnBuoi3/src/btvnbuoi3/bonus.java
+++ btvnBuoi3/src/btvnbuoi3/bonus.java
@@ -4,15 +4,34 @@
  */
 package btvnbuoi3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author NGUYEN TRANG
  */
 public class bonus {
-    static void Fomat(String str){
-        int n = str.length();
-        for(int i=0;i<n;i++){
-            
+
+    static String fomat(String ten) {
+        String[] words = ten.trim().split("\\s+");
+        StringBuilder fomatTen = new StringBuilder();
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                String formatTu = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+                fomatTen.append(formatTu).append(" ");
+            }
         }
+
+        return fomatTen.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập tên: ");
+        String ten = sc.nextLine();
+
+        String formattedName = fomat(ten);
+        System.out.println("Kết quả: " + formattedName);
     }
 }
